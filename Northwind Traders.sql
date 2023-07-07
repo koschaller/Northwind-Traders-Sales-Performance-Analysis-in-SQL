5bd3d28c8cfb9302ede0bff0514b8d9b66465186
Kate <108645447+koschaller@users.noreply.github.com>
2023-07-07 18:52:14 -0400

    Add files via upload

--- Northwind Traders.sql
+++ Northwind Traders.sql
@@ -1,93 +1,127 @@
 /*
 
-Creating Sales and Performance Metrics for Sales Representatives at Northwind Traders
+SALES PERFORMANCE EVALUATION AT NORTHWIND TRADERS
 
-USED: Create Table, Views, CTEs, Aliases, ROUND(), JOINs, Aggregate Functions
+USED: JOINS, AGGREGATE FUNCTIONS, DATE FUNCTIONS, STRING FUNCTIONS
 	  
 */
 
---Create a CTE to display all employee and product details for each order
-CREATE VIEW employee_stats AS 
-WITH revenue AS(
-   SELECT d.orderid AS order_id ,
-          o.employeeid AS employee_id ,
-          e.employeename AS employee_name ,
-          o.customerid AS customer_id ,
-          d.productid AS product_id ,
-          d.unitprice AS unit_price ,
-          d.quantity AS unit_quantity ,
-          d.discount AS discount_rate ,
-          ROUND((d.unitprice*d.quantity)*(1.0-d.discount), 2) AS product_cost
-   FROM order_details d
-   JOIN orders o ON o.orderid = d.orderid
-   JOIN employees e ON o.employeeid = e.employeeid
-   WHERE o.orderdate BETWEEN '2013-12-31' AND '2015-01-01' 
-)
---Use aggregations to calculate the total revenue and count of orders for each employee using the CTE above
-SELECT employee_name ,
-       SUM(product_cost) AS total_revenue ,
-       COUNT(DISTINCT(order_id)) AS total_order_count ,
-       COUNT(DISTINCT(customer_id)) AS count_of_customer_clients
-FROM revenue
-GROUP BY employee_name
-ORDER BY total_revenue DESC
+--What is the annual sales?
+SELECT
+	ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0)
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31';
+--The annual gross sales for 2014 is $617,085
 
 
 
---Creating a view of only the CTE above
-CREATE VIEW order_stats AS
-SELECT d.orderid AS order_id ,
-       o.employeeid AS employee_id ,
-       e.employeename AS employee_name ,
-       o.customerid AS customer_id ,
-       d.productid AS product_id ,
-       d.unitprice AS unit_price ,
-       d.quantity AS unit_quantity ,
-       d.discount AS discount_rate ,
-       ROUND((d.unitprice*d.quantity)*(1.0-d.discount), 2) AS product_cost
-FROM order_details d
-JOIN orders o 
-	ON o.orderid = d.orderid
-JOIN employees e 
-	ON o.employeeid = e.employeeid
-WHERE o.orderdate BETWEEN '2013-12-31' AND '2015-01-01'
+--Who is the top performing sales representative?
+SELECT
+	emp.employeename
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+LEFT JOIN employees AS emp
+		ON ord.employeeid = emp.employeeid
+WHERE DATE_PART('year', ord.orderdate) = 2014
+	AND emp.title = 'Sales Representative'
+GROUP BY emp.employeename
+ORDER BY total_sales DESC;
+--The top performing sales representative is Margaret Peacock with a total of $128,810 in annual sales. 
+
+
+
+--Who is the most profitable customer?
+SELECT
+	cust.companyname
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+LEFT JOIN customers AS cust
+		ON ord.customerid = cust.customerid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY cust.companyname
+ORDER BY total_sales DESC;
+--The most profitable customer is QUICK-Stop with $61,110 in annual sales. 
+
+
+
+
+--What is the top selling product?
+SELECT
+	prod.productname
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+LEFT JOIN products AS prod
+		ON det.productid = prod.productid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY prod.productname
+ORDER BY total_sales DESC;
+--The top selling product is the Cote de Blaye with $49,198 in annual sales. 
+
+SELECT
+	cat.categoryname
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+LEFT JOIN products AS prod
+		ON det.productid = prod.productid
+LEFT JOIN categories AS cat
+		ON cat.categoryid = prod.categoryid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY cat.categoryname
+ORDER BY total_sales DESC;
+--The top selling category is Dairy Products with $115,388 in annual sales. 
+
+
+
+--What is the most profitable region?
+SELECT
+	cust.country
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+LEFT JOIN customers AS cust
+		ON ord.customerid = cust.customerid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY cust.country
+ORDER BY total_sales DESC;
+-- The most profitable country is Germany with $117,320 in annual sales. 
+
+
+
+--Are there seasonality/trends with sales or volume of orders?
+SELECT
+	TO_CHAR(ord.orderdate, 'Month') AS month
+	, ROUND(SUM((det.unitprice*det.quantity)*(1.0-det.discount)), 0) AS total_sales
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY month
+ORDER BY total_sales DESC;
+
+
+SELECT
+	TO_CHAR(ord.orderdate, 'Month') AS month
+	, COUNT(DISTINCT ord.orderid) AS total_volume
+FROM order_details AS det
+LEFT JOIN orders AS ord
+		ON det.orderid = ord.orderid
+WHERE ord.orderdate BETWEEN '2014-01-01' AND '2014-12-31'
+GROUP BY month
+ORDER BY total_volume DESC;
+
+
 		
 		
 
---Calculating total revenue for each employee based on product and category
-CREATE VIEW product_stats AS
-SELECT c.categoryname,
-       p.productid,
-       os.employee_name,
-       SUM(os.product_cost) total_revenue
-FROM products p
-JOIN categories c ON p.categoryid = c.categoryid
-JOIN order_stats os ON os.product_id = p.productid
-GROUP BY c.categoryname,
-         p.productid,
-         os.employee_name
-ORDER BY c.categoryname,
-         p.productid,
-         SUM(os.product_cost) DESC
-
-
-
---Calulating revenue based on client details
-CREATE VIEW company_stats AS
-SELECT cu.companyname AS company_name ,
-       cu.country AS country ,
-       cu.city AS city ,
-       os.employee_name AS employee_name ,
-       SUM(os.product_cost) AS total_revenue
-FROM customers cu
-JOIN order_stats os 
-	ON cu.customerid = os.customer_id
-GROUP BY cu.companyname ,
-         cu.country ,
-         cu.city ,
-         os.employee_name
-ORDER BY cu.companyname ,
-         cu.country ,
-         cu.city ,
-         SUM(os.product_cost)
 	
